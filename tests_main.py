9667e0dd499e46e322ff1720b971f47926c646ed
MSI\rafae <rafa.prats85@gmail.com>
2024-01-15 20:21:13 +0100

    adding test with github actions

--- tests_main.py
+++ tests_main.py
@@ -3,7 +3,7 @@ from main import app
 
 client = TestClient(app)
 
-def testing():
+def tests_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"greetings": "welcome"}
