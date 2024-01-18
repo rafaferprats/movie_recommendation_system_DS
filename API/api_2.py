7a9277e8c5f81abf014de97d75482a9eaef67316
Claudia <claudia.matthes.cm@gmail.com>
2024-01-18 08:48:06 +0100

    added recent changes on API, dbdata, model, metrics

--- API/api_2.py
+++ API/api_2.py
@@ -1,52 +1,37 @@
 import pandas as pd
 import numpy as np
-import pickle
-from fastapi import FastAPI
 from fastapi import Depends, FastAPI, HTTPException, status, Form
 from fastapi.security import HTTPBasic, HTTPBasicCredentials, OAuth2PasswordBearer,OAuth2PasswordRequestForm
-from collections import Counter
 import uvicorn
 from pydantic import BaseModel
 import sqlite3
 from typing_extensions import Annotated
 import datetime
+import json
+
 
 # load the data
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///../data/movie.db', echo=False)
+user = 'root'
+password = 'password'
+host = 'localhost'
+database = 'movie'
+engine = create_engine(f'mysql://{user}:{password}@{host}/{database}')
+import mysql.connector
+
+connection = mysql.connector.connect(host='localhost',
+                                     database='movie',
+                                     user='root',
+                                     password='password')
+
 
 # read data from DB
 ratings_data = pd.read_sql_query("SELECT * FROM ratings", engine)
 movies_data = pd.read_sql_query("SELECT * FROM movies", engine)
+predictions = pd.read_sql_query("SELECT * FROM predictions", engine)
+new_ratings = pd.read_sql_query("SELECT * FROM new_user_rating", engine)
+registered_user = pd.read_sql_query("SELECT * FROM users_db", engine)
 
-# Load the saved model
-with open("../models/svd_model.pkl", 'rb') as file:
-    svd = pickle.load(file)
-
-# Now you can use the model
-def generate_recommendation(model, user_id, ratings_df, movies_df, n_items):
-    # Get a list of all movie IDs from dataset
-    movie_ids = ratings_df["movieId"].unique()
-
-    # Get a list of all movie IDs that have been watched by user
-    movie_ids_user = ratings_df.loc[ratings_df["userId"] == user_id, "movieId"]
-    # Get a list off all movie IDS that that have not been watched by user
-    movie_ids_to_pred = np.setdiff1d(movie_ids, movie_ids_user)
-
-    # Apply a rating of 4 to all interactions (only to match the Surprise dataset format)
-    test_set = [[user_id, movie_id, 4] for movie_id in movie_ids_to_pred]
-
-    # Predict the ratings and generate recommendations
-    predictions = model.test(test_set)
-    pred_ratings = np.array([pred.est for pred in predictions])
-    print("Top {0} item recommendations for user {1}:".format(n_items, user_id))
-    # Rank top-n movies based on the predicted ratings
-    index_max = (-pred_ratings).argsort()[:n_items]
-    reco_list = []
-    for i in index_max:
-        movie_id = movie_ids_to_pred[i]
-        reco_list.append((movies_df[movies_df["movieId"] == movie_id]["title"].values[0], pred_ratings[i]))
-    return reco_list
 class User(BaseModel):
     username: str
     password: str
@@ -57,21 +42,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI(
     title="Movie Recommender API",
     description="API build up by Fernandez, Guillherme and Claudia powered by FastAPI.",
-    version="1.0.1",
-    openapi_tags=[
-        {
-            'name': 'home',
-            'description': 'entry of the API'
-        },
-        {
-            'name': 'movie_recommendation_via_user',
-            'description': 'put the userId and get Top 5 recommended movies'
-        },
-        {
-            'name': 'movie_recomendation_via_movie',
-            'description': 'put the movieId and get Top 5 recommended movies'
-        }
-    ]
+    version="1.0.1"
 )
 
 @app.get('/', tags=["home"])
@@ -80,9 +51,8 @@ def get_index():
     """
     return {'greetings': 'welcome'}
 
-
 @app.get("/movie_recommendation_via_user/{userId}", tags=["movie_recommendation_via_userId"])
-def read_item(token: str = Depends(oauth2_scheme)):
+def get_movie_reco(token: str = Depends(oauth2_scheme)):
     """
     In this endpoint the user will put the userId into the API and will return 5 movies
     which are most likely similiar rated by users therefore in the same cluster
@@ -90,23 +60,15 @@ def read_item(token: str = Depends(oauth2_scheme)):
     :return:
     """
     # generate recommendation using the model that we have trained
-    prediction = generate_recommendation(svd, token, ratings_data, movies_data, 5)
-    print(prediction)
-    return [i[0] for i in prediction]
+    user_prediction = predictions[predictions.userId == int(token[0])]
+    user_prediction = user_prediction.sort_values(by="prediction",ascending=False)
+    choice = user_prediction[:6]
+    movies_data["movieId"] = movies_data["movieId"].astype(int)
+    merged_choice = pd.merge(choice, movies_data, left_on='movieId', right_on='movieId', how='left')
+    res = merged_choice[["movieId", "title"]].to_json(orient="records")
+    parsed = json.loads(res)
+    return parsed
 
-@app.get("/movie_recomendation_via_movie/{movieId}", tags=["movie_recommendation_via_movieID"])
-def read_item(input_movie: int):
-    """
-    In this endpoint the user will put the movieId into the API and will return 5 movies
-    which are similiar rated and therefore in the same cluster
-    :param input_movie:
-    :return:
-    """
-    input_movie = int(input_movie)
-    #list of movies out of reco
-    #list of recos -> ratings
-    # return movie_reco_list
-    pass
 
 ### user registration process  #####
 def get_user(username: str):
@@ -118,17 +80,9 @@ def get_user(username: str):
     if username in users_list:
         return username
 
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(token)
-    return user
-
-def fake_hash_password(password: str):
-    return password
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
+    user = token[0]
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -136,7 +90,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
             headers={"WWW-Authenticate": "Bearer"},
         )
     return user
-
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.disabled:
@@ -180,21 +133,15 @@ def all_user(token: str = Depends(oauth2_scheme)):
 def set_new_user(user: User, token: str = Depends(oauth2_scheme)):
     print(token)
     if token=="admin":
-        connection = sqlite3.connect("../data/movie.db")
         cursor = connection.cursor()
-        print(user.username)
-        query = f"INSERT INTO  users_db(username, password) VALUES (?, ?)"
-        cursor.execute(query, (user.username, user.password))
-        connection.commit()
+        if len(registered_user[registered_user["username"]==str(token[0])])==0:
+            query = f"INSERT INTO  users_db(username, password) VALUES (%s, %s)"
+            cursor.execute(query, (user.username, user.password))
+            connection.commit()
+        else:
+            raise HTTPException(status_code=400, detail="user already exists")
     if token!= "admin":
             raise HTTPException(status_code=400, detail="not sufficient rights")
-
-class Ratings(BaseModel):
-    rating_1: int
-    rating_2: int
-    rating_3: int
-    rating_4: int
-    rating_5: int
 
 @app.put("/ratings/", name='register new user ratings')
 def set_ratings_to_recommended_movies(rating_1: Annotated[str, Form()],
@@ -203,37 +150,54 @@ def set_ratings_to_recommended_movies(rating_1: Annotated[str, Form()],
                                       rating_4: Annotated[str, Form()],
                                       rating_5: Annotated[str, Form()],
                                       token: str = Depends(oauth2_scheme)):
-    prediction = generate_recommendation(svd, token, ratings_data, movies_data, 5)
-    prediction_movie_title = [i[0] for i in prediction]
-    #sql append
-    #[userid, movieid, rating]
-    pred_ratings = [i[1] for i in prediction]
-    user_ratings = [rating_1, rating_2, rating_3, rating_4, rating_5]
-    movieid = [movies_data[movies_data["title"] == movie]["movieId"].item() for movie in prediction_movie_title]
-    test = []
-    rating_data_input= []
-    for i in range(0,len(pred_ratings)):
-        rating_list_for_db = [token[0], movieid[i], prediction_movie_title[i], pred_ratings[i], user_ratings[i]]
-        test.append(rating_list_for_db)
 
-    test_df = pd.DataFrame(test)
-    print(test_df)
-    test_df.to_sql('new_user_rating', con=engine, if_exists='append', index=False)
+    user_prediction = predictions[predictions.userId == int(token[0])]
+    user_prediction = user_prediction.sort_values(by="prediction", ascending=False)
+    choice = user_prediction[:5]
+    ratings_data.rename(columns={"user_id": "userId", "item_id": "movieId"}, inplace=True)
+    new_ratings["movieId"] = new_ratings["movieId"].astype(int)
+    print("new_ratings", new_ratings)
+    df = pd.merge(choice[["userId", "movieId"]], new_ratings[["userId", "movieId", "title"]], on=["userId", "movieId"],
+                  how='left', indicator='Exist')
+    df['Exist'] = np.where(df.Exist == 'both', True, False)
+    for i in range(0, len(df)):
+        if df.iloc[i]["Exist"] == True:
+            raise HTTPException(status_code=400, detail=f"you've rated already your recommendation")
+        else:
+            merged_choice = pd.merge(choice, movies_data, left_on='movieId', right_on='movieId', how='left')
+            print(merged_choice)
+            prediction_movie_title = [merged_choice.iloc[i]["title"] for i in range(0,len(merged_choice))]
+            print(prediction_movie_title)
+            #sql append
+            #[userid, movieid, rating]
+            pred_ratings = [choice.iloc[i].prediction for i in range(0,len(choice))]
+            user_ratings = [rating_1, rating_2, rating_3, rating_4, rating_5]
+            movieid = [choice.iloc[i].movieId for i in range(0,len(choice))]
+            test = []
+            rating_data_input= []
+            for i in range(0,len(pred_ratings)):
+                rating_list_for_db = [token[0], movieid[i], prediction_movie_title[i], pred_ratings[i], user_ratings[i]]
+                test.append(rating_list_for_db)
 
-    for i in range(0,len(pred_ratings)):
-        rating_list_for_db = [token[0], movieid[i], user_ratings[i], datetime.datetime.now()]
-        rating_data_input.append(rating_list_for_db)
-    rating_data_input_df = pd.DataFrame(rating_data_input)
-    rating_data_input_df.to_sql('ratings', con=engine, if_exists='append', index=False)
+            test_df = pd.DataFrame(test, columns=["userId", "movieId", "title", "preds_rating", "rating"])
+            print(test_df)
+            cursor = connection.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS new_user_rating(userId INT, movieId INT, title VARCHAR(120), preds_rating FLOAT, rating FLOAT)")
+            test_df.to_sql('new_user_rating', con=engine, if_exists='append', index=False)
 
+            for i in range(0,len(pred_ratings)):
+                rating_list_for_db = [token[0], movieid[i], user_ratings[i], datetime.datetime.now()]
+                rating_data_input.append(rating_list_for_db)
+            rating_data_input_df = pd.DataFrame(rating_data_input, columns=["user_id", "item_id", "rating", "timestamp"])
+            rating_data_input_df.to_sql('ratings', con=engine, if_exists='append', index=False)
 
-    return {f"{prediction_movie_title[0]}": rating_1,
-            f"{prediction_movie_title[1]}": rating_2,
-            f"{prediction_movie_title[2]}": rating_3,
-            f"{prediction_movie_title[3]}": rating_4,
-            f"{prediction_movie_title[4]}": rating_5}
+        return {f"{prediction_movie_title[0]}": rating_1,
+                f"{prediction_movie_title[1]}": rating_2,
+                f"{prediction_movie_title[2]}": rating_3,
+                f"{prediction_movie_title[3]}": rating_4,
+                f"{prediction_movie_title[4]}": rating_5}
 
 
 if __name__ == "__main__":
     # Run the FastAPI application on an Uvicorn server
-    uvicorn.run(host="0.0.0.0")
+    uvicorn.run(app,host="0.0.0.0")
--- data/db.py
+++ data/db.py
@@ -1,29 +1,67 @@
+import os
 import pandas as pd
-import datetime
-
-import sqlite3
-connection = sqlite3.connect("movie.db")
+from d2l import mxnet as d2l
 
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///movie.db', echo=False)
+import mysql.connector
+import datetime
 
+# Replace these with your actual database credentials
+user = 'root'
+password = 'password'
+host = 'localhost'
+database = 'movie'
+
+# Create the MySQL engine
+engine = create_engine(f'mysql://{user}:{password}@{host}/{database}')
+
+connection = mysql.connector.connect(host='localhost',
+                                         database='movie',
+                                         user='root',
+                                         password='password')
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS users_db(username, password)")
+cursor.execute("""CREATE TABLE IF NOT EXISTS  users_db(
+username VARCHAR(255) not null, 
+password VARCHAR(255) not null);""")
 
 cursor.execute("INSERT INTO users_db VALUES ('admin', 'admin')")
 cursor.execute("INSERT INTO users_db VALUES ('1', 'password')")
 
 connection.commit()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS movies(movieId,title,genres)")
-movies=pd.read_csv("./ml-latest-small/movies.csv")
-movies.to_sql('movies', con=engine, if_exists='replace', index=False)
 
-#
-#
-cursor.execute("CREATE TABLE IF NOT EXISTS ratings(userId,movieId,rating,timestamp)")
-ratings = pd.read_csv("./ml-latest-small/ratings.csv")
-ratings["timestamp"] = datetime.datetime.now()
+#@save
+d2l.DATA_HUB['ml-100k'] = (
+    'https://files.grouplens.org/datasets/movielens/ml-100k.zip',
+    'cd4dcac4241c8a4ad7badc7ca635da8a69dddb83')
 
-ratings.to_sql('ratings', con=engine, if_exists='replace', index=False)
+#@save
+def read_data_ml100k():
+    data_dir = d2l.download_extract('ml-100k')
+    names = ['user_id', 'item_id', 'rating', 'timestamp']
+    data = pd.read_csv(os.path.join(data_dir, 'u.data'), sep='\t',
+                       names=names, engine='python')
+    names_ = ['movieId', 'title', 'genre', "release date" , "video release date" ,
+              "IMDb URL" , "unknown" ,"Action" , "Adventure" , "Animation" ,
+              "Children's" , "Comedy" , "Crime" , "Documentary" , "Drama" , "Fantasy" ,
+              "Film-Noir" , "Horror" , "Musical" , "Mystery" , "Romance" , "Sci-Fi" ,
+              "Thriller" , "War" , "Western" ]
+    data_movie = pd.read_csv(os.path.join(data_dir, 'u.item'), sep='|',encoding="ISO-8859-1",
+                       names=names_, engine='python')
+    num_users = data.user_id.unique().shape[0]
+    num_items = data.item_id.unique().shape[0]
+    return data,data_movie, num_users, num_items
+
+data,data_movie, num_users, num_items = read_data_ml100k()
+sparsity = 1 - len(data) / (num_users * num_items)
+print(f'number of users: {num_users}, number of items: {num_items}')
+print(f'matrix sparsity: {sparsity:f}')
+data["timestamp"] = datetime.datetime.now()
+# print(data.head(5))
+
+# data.to_sql('ratings', con=engine, if_exists='replace', index=False)
+data_movie = data_movie[['movieId', 'title']]
+# print(data_movie.head())
+data_movie.to_sql('movies', con=engine, if_exists='replace', index=False)
+
